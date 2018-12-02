70dde85dc24f7b0f9c6a04a9d658cee7e5bc4c7a
Michael Anthony <mianthony404@gmail.com>
2018-12-02 17:41:05 -0500

    Add files via upload

---
+++ Code/MapKitGetUserLoc/MapKitGetUserLoc/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  MapKitGetUserLoc
+//
+//  Created by Michael Anthony on 12/2/18.
+//  Copyright © 2018 Michael Anthony. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
+
+  var locationManager = CLLocationManager()
+  
+  
+  
+  @IBOutlet weak var map: MKMapView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.locationManager.requestWhenInUseAuthorization()
+    
+    if CLLocationManager.locationServicesEnabled() {
+      
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+      locationManager.startUpdatingLocation()
+      
+      
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+    print("locations = \(locValue.latitude) \(locValue.longitude)")
+    let userLocation = locations.last
+    let viewRegion = MKCoordinateRegion(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
+    self.map.setRegion(viewRegion, animated: true)
+  }
+  
+}
